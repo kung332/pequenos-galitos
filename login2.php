7a8a861c522591bd92dfaee26c04bfe8ec71a35f
kung332 <125404332+kung332@users.noreply.github.com>
2023-04-05 16:16:11 -0300

    o inferno

--- login2.php
+++ login2.php
@@ -1,3 +1,37 @@
+<?php
+
+include "conexao.php";
+
+// Inicializa a sessão
+session_start();
+
+// Verifica se o formulário de login foi submetido
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtém o nome de usuário e senha do formulário
+    $username = mysqli_real_escape_string($conn, $_POST["usuario"]);
+    $password = mysqli_real_escape_string($conn, $_POST["senha"]);
+
+    // Consulta o banco de dados para verificar se o usuário e senha correspondem a um registro
+    $sql = "SELECT id FROM usuarios WHERE nome = '$username' AND senha = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    // Verifica se a consulta retornou algum resultado
+    if (mysqli_num_rows($result) == 1) {
+        // Inicia a sessão com o ID do usuário
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION["user_id"] = $row["id"];
+
+        // Redireciona para a página de cadastro
+        header("Location: cadastro.php");
+        exit();
+    } else {
+        // Exibe uma mensagem de erro caso o usuário e senha não correspondam a um registro
+        $login_error = "Nome de usuário ou senha inválidos.";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt.br">
 <head>
