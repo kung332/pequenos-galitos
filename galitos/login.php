729154bb5a16bb7ce33e5249ef75dd8a147ba59f
kung332 <125404332+kung332@users.noreply.github.com>
2023-04-05 17:30:45 -0300

    modificação

--- galitos/login.php
+++ galitos/login.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = mysqli_real_escape_string($conn, $_POST["senha"]);
 
     // Consulta o banco de dados para verificar se o usuário e senha correspondem a um registro
-    $sql = "SELECT id FROM login WHERE nome = '$username' AND senha = '$password'";
+    $sql = "SELECT id FROM usuarios WHERE nome = '$username' AND senha = '$password'";
     $result = mysqli_query($conn, $sql);
 
     // Verifica se a consulta retornou algum resultado
