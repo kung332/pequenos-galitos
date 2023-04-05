08c25b6978c4c170b7881c21cd298c9b97e8a648
kung332 <125404332+kung332@users.noreply.github.com>
2023-04-05 17:43:29 -0300

    modificação

---
+++ galitos/cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busca na agenda</title>
+</head>
+<body>
+    <h1>Busca na agenda</h1>
+
+    <!-- Formulário HTML -->
+<form action="" method="post">
+  <label>Nome: </label><br>
+  <input type="text" name="nome"><br>
+  <label>Sobrenome: </label><br>
+  <input type="text" name="sobrenome"><br>
+  <label>Telefone: </label><br>
+  <input type="text" name="telefone"><br>
+  <input type="submit" value="Gravar">
+</form>
+
+
+</body>
+</html>
